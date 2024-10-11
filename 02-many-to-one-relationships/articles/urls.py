ce11ea943da8e93add507eaba4b8284f3aa39d95
garam0107 <name3937@gmail.com>
2024-10-11 10:57:13 +0900

    1011_live

--- 02-many-to-one-relationships/articles/urls.py
+++ 02-many-to-one-relationships/articles/urls.py
@@ -9,4 +9,7 @@ urlpatterns = [
     path('create/', views.create, name='create'),
     path('<int:pk>/delete/', views.delete, name='delete'),
     path('<int:pk>/update/', views.update, name='update'),
+    path('<int:pk>/comment/', views.comment_create, name = 'comment_create'),
+    path('<int:article_pk>/comment/<int:comment_pk>/delete/', views.comment_delete, name = 'comment_delete'),
+
 ]
--- 02-many-to-one-relationships/articles/views.py
+++ 02-many-to-one-relationships/articles/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 
-from .models import Article
-from .forms import ArticleForm
+from .models import Article,Comment
+from .forms import ArticleForm,CommentForm
 
 
 # Create your views here.
@@ -16,8 +16,12 @@ def index(request):
 
 def detail(request, pk):
     article = Article.objects.get(pk=pk)
+    comment_form = CommentForm()
+    comments = article.comment_set.all()
     context = {
         'article': article,
+        'comment_form' : comment_form,
+        'comments' : comments,
     }
     return render(request, 'articles/detail.html', context)
 
@@ -59,3 +63,36 @@ def delete(request, pk):
     article = Article.objects.get(pk=pk)
     article.delete()
     return redirect('articles:index')
+
+
+def comment_create(request, pk):
+    article = Article.objects.get(pk = pk)
+    comment_form = CommentForm(request.POST)
+    if comment_form.is_valid():
+        # 외래 키 데이터를 넣는 타이밍이 필요
+        # 외래 키를 넣으러면 2가지 조건이 필요
+        # 1. comment 인스턴스 필요
+        # 2. save 메서드가 호출 되기 전이어야 함
+        # 하지만 comment 인스턴스가 생성되는 조건은 save 메서드가 먼저 호출되어야함
+        # django의 save 메서드는 인스턴스만 제공하고, 실제 저장은 잠시 대기하는 옵션을 제공
+        # commit = False 옵션을 주면 저장은 하지 않고 인스턴스만 return
+        comment = comment_form.save(commit=False)
+        comment.article = article
+        comment.save()
+        return redirect('articles:detail', article.pk)
+    context = {
+        'article' : article,
+        'comment_form' : comment_form,
+    }
+    return render(request , 'articles/detail.html' , context)
+
+def comment_delete(request,article_pk,comment_pk):
+    
+    comment = Comment.objects.get(pk=comment_pk)
+    # article의 pk를 가져오는 첫번 째 방법
+    #article_pk = comment.article.pk
+    # article의 pk를 가져오는 두번 째 방법
+    # urls에서 pk를 2개 가져오가
+    article = Article.objects.get(pk=article_pk)
+    comment.delete()
+    return redirect('articles:detail', article.pk)
